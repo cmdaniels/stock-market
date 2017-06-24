e1cfbe75b2ce93028759ca0cd59e28e425f6bc4e
Cody Daniels <codydaniels@gmail.com>
2017-06-23 17:19:19 -0700

    Setup stock api

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var yahooStocks = require('yahoo-stocks');
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
+app.post('/yahoo', function(req, res) {
+  var responses = [];
+  var i = 0;
+  req.body.stocks.split(',').forEach(function(stock) {
+    yahooStocks.history(stock, {
+      interval: '5d'
+    }).then(function(response) {
+      response.ticker = stock;
+      responses.push(response);
+      i++;
+      if(i === req.body.stocks.split(',').length){
+        res.send(responses);
+      }
+    });
+  });
+});
+
 app.listen(process.env.PORT || 3000, function () {
-  console.log('Stock Market listening on port ' + process.env.PORT || 3000 + '!');
+  console.log('Stock Market listening on port ' + (process.env.PORT || 3000) + '!');
 });
